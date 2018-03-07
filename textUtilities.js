5fc6f736acb4c27639d5c5b62cb6bf0d3de6c9d4
ingunnrob <ingunnrob@gmail.com>
2018-03-07 10:22:34 +0000

    Greater expectations added from TT

--- textUtilities.js
+++ textUtilities.js
@@ -1,9 +1,8 @@
 var expect = require('chai').expect;
 
-expect(true).to.be.true;
-
 function titleCase(title) {
     return title;
 }
 
 expect(titleCase('The Great Mouse Detective')).to.be.a('string');
+expect(titleCase('The Great Mouse Detective'));
